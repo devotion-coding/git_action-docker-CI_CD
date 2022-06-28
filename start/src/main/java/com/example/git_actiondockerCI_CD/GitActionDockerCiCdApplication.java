1d30980aa331d4fc1b48e24169ae8bdb1986f7bf
tianjie <tianjie@xueqiu.com>
2022-06-28 14:22:41 +0800

    初始化

---
+++ start/src/main/java/com/example/git_actiondockerCI_CD/GitActionDockerCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.example.git_actiondockerCI_CD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActionDockerCiCdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitActionDockerCiCdApplication.class, args);
+    }
+
+}
---
+++ start/src/test/java/com/example/git_actiondockerCI_CD/GitActionDockerCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_actiondockerCI_CD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionDockerCiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
